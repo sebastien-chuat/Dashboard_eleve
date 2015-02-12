701066dc3963955874001ee398a1255c3b2c34ed
Sébastien Chuat <sebastien.chuat@gmail.com>
2015-02-12 13:34:11 +0100

    Nouvelles pages crées

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('dashboard.views',
     url(r'^$', 'home'),  # Accueil du dashboard
     url(r'^course/(?P<id_course>\d+)$', 'view_course'),  # Vue d'un cours
-    url(r'^courses/(?P<year>\d{4})/(?P<month>\d{2})$', 'list_articles'),  # Vue des courss d'une branche précise
+    url(r'^cours', 'list_cours'),  # Vue des cours d'une branche précise
     url(r'^date$', 'date_actuelle')
 )
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,31 +4,21 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 def home(request):
-    """ Exemple de page HTML, non valide pour que l'exemple soit concis """
-    text = """
-    <h1>Bienvenue sur Webmath !</h1>
-    <p>Ici sera présent le Dashboard</p>"""
-    return HttpResponse(text)
+    return render(request, 'dashboard/home.html')
 
 def view_course(request, id_course):
-    """ Vue qui affiche un article selon son identifiant (ou ID, ici un numéro)
-        Son ID est le second paramètre de la fonction (pour rappel, le premier
-        paramètre est TOUJOURS la requête de l'utilisateur) """
+    """ je dois trouver comment afficher un cours ici"""
 
-    text = "Vous avez demandé le cours #{0} !".format(id_course)
-    if int(id_course) > 100:
-    	raise Http404
-    return HttpResponse(text)
+    return render(request, '')
 
-def list_articles(request, month, year):
-    """ Liste des articles d'un mois précis. """
+def list_cours(request):
+    """ Liste des cours. """
 
-    text = "Vous avez demandé les articles de {0} {1}.".format(month, year)
-    return HttpResponse(text)
+    return render (request, 'dashboard/cours.html')
 
 
 
 ## ICI c'est vraiment à quoi ça doit ressembler ! Et non pas avec des HttpResponse ##
 
 def date_actuelle(request):
-    return render (request, 'dashboard/date.html', {'date': datetime.now()})
+    return render (request, 'dashboard/date.html')
